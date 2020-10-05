8e930e9936e54067f0d2923b9c6017de0e2ce9f8
michaelA1020 <70971720+michaelA1020@users.noreply.github.com>
2020-10-04 23:26:43 -0400

    Switched some vars "mbr" to "ir" to better reflect fetch cycle

--- 6461Computer/src/Project6461/Word.java
+++ 6461Computer/src/Project6461/Word.java
@@ -6,38 +6,38 @@ public class Word {
 	protected short reg2;
 	protected short address;
 	
-	public Word(short mbr) {
-		opcode = getOpcode(mbr);
-		reg1 = getReg1(mbr);
-		reg2 = getReg2(mbr);
-		address = getAddress(mbr);
+	public Word(short ir) {
+		opcode = getOpcode(ir);
+		reg1 = getReg1(ir);
+		reg2 = getReg2(ir);
+		address = getAddress(ir);
 		debugging();
 	}
 	
-	private short getOpcode(short mbr) {
-		short operationcode = mbr;
+	private short getOpcode(short ir) {
+		short operationcode = ir;
 		operationcode = (short) (operationcode >> 10); //remove everything else from the word to get the opcode
 		return operationcode;
 	}
 	
-	private short getReg1(short mbr) {
+	private short getReg1(short ir) {
 		//TODO calculating wrong values
-		short reg1 = mbr;
+		short reg1 = ir;
 		reg1 = (short) (reg1 << 6);
 		reg1 = (short) (reg1 >> 13);
 		return reg1;
 	}
 	
-	private short getReg2(short mbr) {
+	private short getReg2(short ir) {
 		//TODO calculating wrong values?
-		short reg2 = mbr;
+		short reg2 = ir;
 		reg2 = (short) (reg2 << 8);
 		reg2 = (short) (reg2 >> 13);
 		return reg2;
 	}
 	
-	private short getAddress(short mbr) {
-		short addr = mbr;
+	private short getAddress(short ir) {
+		short addr = ir;
 		addr = (short) (addr << 11);
 		addr = (short) (addr >> 11);
 		return addr;
@@ -49,5 +49,4 @@ public class Word {
 		System.out.println("Reg2=" + reg2);
 		System.out.println("Addr=" + address);
 	}
-
 }
